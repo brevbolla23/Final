982b814b1ff6da756571e6f0f3f285aeeb9989bf
brevbolla23 <47837059+brevbolla23@users.noreply.github.com>
2019-08-22 08:19:30 -0400

    a

---
+++ gem.js
@@ -0,0 +1,11 @@
+function greeting() {
+  var greeting = "Planning on a trip to Oldham County?";
+  console.log(greeting);
+  
+}
+
+
+<form name="myform" action="submit-form.ph">;
+Search: <input type='text' name='query'/>;
+<input type="image" src="go.gif"/>;
+</form>
